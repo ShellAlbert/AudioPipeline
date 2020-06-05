128b4bdfda65891dd4a9aa7a54ef8c8265fc437a
ShellAlbert <ShellAlbert@localhost.localdomain>
2020-06-05 14:05:25 +0800

    add killall script.

--- killall.sh
+++ killall.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
-for pid in `ls /tmp/zsy/*.pid` 
+
+
+#for pid in `ls /tmp/zsy/*.pid` 
+#do
+#	echo $pid
+#	kill -9 `cat $pid`
+#done
+
+
+ps aux | grep a.sh | awk '{print $2}' | while read pid
 do
-	echo $pid
-	kill -9 `cat $pid`
+	echo "kill pid:" $pid
+	kill -9 $pid
 done
+
+ps aux | grep zns | awk '{print $2}' | while read pid
+do
+	echo "kill pid:" $pid
+	kill -9 $pid
+done
+
